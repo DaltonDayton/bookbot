1f4cb1229736606791871d45968386a7ec7b2544
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-03-12 18:22:58 -0600

    count letters

--- main.py
+++ main.py
@@ -9,12 +9,30 @@ def count_words(text):
     return len(words)
 
 
+def count_characters(text):
+    words = text.split()
+    char_dict = {}
+    for word in words:
+        word = word.lower()
+        for char in word:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    return dict(sorted(char_dict.items()))
+
+
 def main():
     text = read_book("books/frankenstein.txt")
     print(text)
 
     words = count_words(text)
-    print(words)
+    print(f"There are { words } words in this book.\n")
+
+    print("The character counts in this book are: ")
+    characters = count_characters(text)
+    for char in characters:
+        print(char, characters[char])
 
 
 main()
